0a1be15b6cbd5d698e0f93da17785e0e372a6b62
rido zaen <ridozaen@Rido-MacBook-Pro.local>
2018-06-07 23:22:26 +0700

    added backend user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userController.getAlluser);
+router.get('/:id', userController.getUserById);
+/* Update users */
+router.put('/:id', userController.updateUser);
+// deactivated user 
+router.post('/deactivated', userController.deactivatedUser);
+
+// activated user 
+router.post('/activated', userController.activatedUser)
 
 module.exports = router;
