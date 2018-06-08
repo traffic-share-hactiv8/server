f4a872445cd4b0f06a53ef12383bdbeb5ca2c09b
rido zaen <ridozaen@irnas-MBP-2.localdomain>
2018-06-08 11:02:54 +0700

    modify route timeline to add uploadgsc storage and timeline.populate()

--- routes/timelines.js
+++ routes/timelines.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const images         = require('../middleware/upload');
 const timelineController = require('../controllers/timelineController');
 
 /* GET time line */
 router.get('/',timelineController.getAllTimeline);
-router.post('/',timelineController.createTl);
+router.post('/',images.multer.single('image'), images.sendUploadToGCS,timelineController.createTl);
 router.get('/currentUser',timelineController.getTLCurrentUser);
 router.put('/:id',timelineController.editTL);
 
